6a7799fc636e018125f74a129a9ffa545fb4b869
Vladislav Sadretdinov <vlad.sadretdinov@gmail.com>
2018-10-28 08:15:28 +0300

    Add mobile style to catalog.html

--- source/js/script.js
+++ source/js/script.js
@@ -3,12 +3,28 @@ var modal__overlay = document.querySelector(".modal__overlay");
 var order = document.querySelector(".featured__btn");
 var main_nav__toggle = document.querySelector(".main-nav__toggle");
 var main_nav__list = document.querySelector(".main-nav__list");
+var order__autofocus = document.querySelector(".order__label[for='order_size-s']");
+var product__btns = document.querySelectorAll(".product__button");
 
-order.addEventListener("click", function (event) {
-  event.preventDefault();
-  modal__overlay.classList.add("modal__overlay--show");
-  modal.classList.add("modal--show");
-});
+if(order !== null) {
+  order.addEventListener("click", function (event) {
+    event.preventDefault();
+    modal__overlay.classList.add("modal__overlay--show");
+    modal.classList.add("modal--show");
+    order__autofocus.focus();
+  });
+}
+
+if(product__btns !==null ) {
+  for (var i=0, iLen=product__btns.length; i<iLen; i++) {
+    product__btns[i].addEventListener("click", function (event) {
+      event.preventDefault();
+      modal__overlay.classList.add("modal__overlay--show");
+      modal.classList.add("modal--show");
+      order__autofocus.focus();
+    });
+  }
+}
 
 modal__overlay.addEventListener("click", function (event) {
   if (event.target == modal__overlay) {
@@ -28,4 +44,12 @@ main_nav__toggle.addEventListener("click", function (event) {
 
   main_nav__toggle.classList.add("main-nav__toggle--open");
   main_nav__list.classList.add("main-nav__list--open");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    modal.classList.toggle("modal--show",false);
+    modal__overlay.classList.remove("modal__overlay--show");
+  }
 });
